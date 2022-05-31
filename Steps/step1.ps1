5f87cb097f9c6479e57258b138e001d8b733c6d5
ChocolateMoo53 <54446699+ChocolateMoo53@users.noreply.github.com>
2022-05-30 23:08:05 -0700

    Fixed file path error

--- Steps/step1.ps1
+++ Steps/step1.ps1
@@ -101,7 +101,7 @@ GetFile "http://www.download.windowsupdate.com/msdownload/update/v3-19990518/cab
 cmd.exe /c "C:\Windows\System32\expand.exe C:\cloudopenstream\Drivers\xbox360.cab -F:* C:\cloudopenstream\Drivers\" | Out-Null
 Copy-Item -Path $WorkDir\devcon.exe -Destination $specialFolder -Recurse
 cmd.exe /c '"C:\cloudopenstream\devcon.exe" dp_add "C:\cloudopenstream\Drivers\xusb21.inf"' | Out-Null
-Start-Process -FilePath "C:\cloudopenstream\Drivers\vigembus\10\x64\devcon.exe" -ArgumentList '/r disable "HDAUDIO\FUNC_01&VEN_10DE&DEV_0083&SUBSYS_10DE11A3*"'
+Start-Process -FilePath $WorkDir\devcon.exe -ArgumentList '/r disable "HDAUDIO\FUNC_01&VEN_10DE&DEV_0083&SUBSYS_10DE11A3*"'
 Write-Host "Setup for Sunshine has completed!" -ForegroundColor Green
 } 
 

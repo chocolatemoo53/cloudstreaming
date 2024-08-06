c5d16ee8714e19de3f67160c1657626b3096db98
ChocolateMoo53 <54446699+chocolatemoo53@users.noreply.github.com>
2024-08-05 18:52:54 -0700

    Fix spacing between if, revert step 3

--- Steps/step1.ps1
+++ Steps/step1.ps1
@@ -14,7 +14,7 @@ Function GetFile([string]$Url, [string]$Path, [string]$Name) {
 }
 
 $OldVersion = (Read-Host "Would you like to turn off Internet Explorer restrictions on Server 2019 and below? (y/n)").ToLower() -eq "y"
-if ($OldVersion) {
+if($OldVersion) {
 Write-Host "Removing IE restrictions..."
 Set-Itemproperty "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" -name IsInstalled -value 0 -force | Out-Null
 Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}" -Name IsInstalled -Value 0 -Force | Out-Null
@@ -29,21 +29,21 @@ Write-Host "Consult the wiki for more information"
 
 $streamTech = Read-Host -Prompt 'Type the number corresponding your choice'
 
-if ($streamTech -eq 1) {
+if($streamTech -eq 1) {
 Write-Host ""
 GetFile "https://builds.parsecgaming.com/package/parsec-windows.exe" "$WorkDir\parsec.exe" "Parsec"
 Write-Host "Installing Parsec..."
 Start-Process -FilePath "$WorkDir\parsec.exe" -ArgumentList "/norun /silent" -NoNewWindow -Wait -Passthru
 }
 
-if ($streamTech -eq 2) {
+if($streamTech -eq 2) {
 Write-Host ""
 GetFile "https://d1uj6qtbmh3dt5.cloudfront.net/nice-dcv-server-x64-Release.msi" "$specialFolder\nicedcv.msi" "NiceDCV" 
 Write-Host "Installing NiceDCV..."
 Start-Process -FilePath "msiexec.exe" -Wait -ArgumentList '/qn /i C:\cloudstreaming\nicedcv.msi'
 }
 
-if ($streamTech -eq 3) {
+if($streamTech -eq 3) {
 Write-Host ""
 GetFile "https://github.com/LizardByte/Sunshine/releases/latest/download/sunshine-windows-installer.exe" "$WorkDir\sunshine.exe" "Sunshine"
 Write-Host "Installing Sunshine..."
@@ -66,7 +66,7 @@ Start-Process cmd.exe -ArgumentList "/c C:\cloudstreaming\ResolutionMatcher\inst
 Write-Host "ResolutionAutomation and Sunshine installed successfully!" -ForegroundColor Green
 } 
 
-if ($streamTech -in 1, 3) {
+if($streamTech -in 1, 3) {
 Write-Host ""
 $Audio = (Read-Host "Would you like to download audio drivers? (y/n)").ToLower() -eq "y"
 if($Audio) { 
@@ -78,7 +78,7 @@ Import-Certificate -FilePath "C:\cloudstreaming\vbcable.cer" -CertStoreLocation 
 Start-Process -FilePath "$WorkDir\vbcable\VBCABLE_Setup_x64.exe" -ArgumentList "-i","-h" -NoNewWindow -Wait }
 }
 
-if ($streamTech -in 1, 3) {
+if($streamTech -in 1, 3) {
 Write-Host ""
 $Monitor = (Read-Host "You may need a headless display/monitor (requires 2022). Would you like to install? (y/n)").ToLower() -eq "y"
 if($Monitor) {
@@ -86,16 +86,17 @@ Start-Process -FilePath "$WorkDir\sunshine.exe" -ArgumentList "/s" -NoNewWindow 
 Write-Host "Getting the headless display driver and monitor"
 GetFile "https://github.com/itsmikethetech/Virtual-Display-Driver/releases/download/23.12.2HDR/VDD.HDR.23.12.zip" "$WorkDir\idd.zip"
 Expand-Archive -Path "$WorkDir\idd.zip" -DestinationPath "$specialFolder\idd" | Out-Null
-Copy-Item -Path "$specialFolder\idd\IddSampleDriver\option.txt" -Destination "$vddFolder\option.txt" | Out-Null
+Copy-Item -Path "$specialFolder\idd\VDD HDR 23.12.2\IddSampleDriver\option.txt" -Destination "$vddFolder\option.txt" | Out-Null
 Start-Process cmd.exe -ArgumentList "/c C:\cloudstreaming\idd\IddSampleDriver\InstallCert.bat"
 Write-Host "Now install the display driver using add legacy hardware in device manager."
 Write-Host "Select display adapters, then have disk, then browse to c:\cloudstreaming\idd\IddSampleDriver\IddSampleDriver.inf"
-Write-Host "While you are there, you will want to disable the Microsoft Basic Display Adapter." }
+Write-Host "While you are there, you will want to disable the Microsoft Basic Display Adapter."
+Write-Host "" }
 }
 
 $Video = (Read-Host "Would you like to install video drivers (AWS and GCP)?").ToLower() -eq "y"
  
-if ($Video) {
+if($Video) {
 $Shell = New-Object -comObject WScript.Shell
 $Shortcut = $Shell.CreateShortcut("$Home\Desktop\Continue.lnk")
 $Shortcut.TargetPath = "powershell.exe"
